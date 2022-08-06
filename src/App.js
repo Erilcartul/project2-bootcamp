fe5cf6ff26d817869316bca8c342cad0724b0ee2
Erilcartul <Fabiankhoo517@gmail.com>
2022-08-06 11:12:48 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,57 @@
+import { onAuthStateChanged } from "firebase/auth";
 import React from "react";
-import logo from "./logo.png";
 import "./App.css";
+import AuthForm from "./Authentication";
+import Composer from "./Main";
+import { auth } from "./Firebase";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedInUser: null,
+      shouldRenderAuthForm: false,
+    };
+  }
+
+  componentDidMount() {
+    onAuthStateChanged(auth, (user) => {
+      // If user is logged in, save logged-in user to state
+      if (user) {
+        this.setState({ loggedInUser: user });
+        return;
+      }
+      // Else set logged-in user in state to null
+      this.setState({ loggedInUser: null });
+    });
+  }
+
+  toggleAuthForm = () => {
+    this.setState((state) => ({
+      shouldRenderAuthForm: !state.shouldRenderAuthForm,
+    }));
+  };
+
   render() {
+    const authForm = <AuthForm toggleAuthForm={this.toggleAuthForm} />;
+    const composer = <Composer loggedInUser={this.state.loggedInUser} />;
+    const createAccountOrSignInButton = (
+      <div>
+        <button onClick={this.toggleAuthForm}>Create Account Or Sign In</button>
+        <br />
+      </div>
+    );
+    const composerAndNewsFeed = (
+      <div>
+        {/* Render composer if user logged in, else render auth button */}
+        {this.state.loggedInUser ? composer : createAccountOrSignInButton}
+      </div>
+    );
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <br />
+          {this.state.shouldRenderAuthForm ? authForm : composerAndNewsFeed}
         </header>
       </div>
     );
